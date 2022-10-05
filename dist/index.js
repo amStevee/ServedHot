223bb589c273026b6984a19cb501b22529d178a7
amStevee <stevenanongo2@gmail.com>
2022-10-04 17:26:38 -0700

    added scss and typscript

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let user = {
+    id: 2,
+    name: "steven",
+    role: "CEO",
+    age: 21,
+};
+console.log(user.name);
+function getTax(tax) {
+    console.log(tax);
+    return tax * 1.2;
+}
+console.log(getTax(300000));
+//# sourceMappingURL=index.js.map

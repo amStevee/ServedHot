688e87926a3333531aecd46c8db96a07adb63033
amStevee <stevenanongo2@gmail.com>
2022-10-13 16:36:32 -0700

    min

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// import "dotenv/config";
 import axios from "axios";
 
 const slides = document.getElementById("slides") as HTMLDivElement | null,
@@ -5,13 +6,14 @@ const slides = document.getElementById("slides") as HTMLDivElement | null,
   search_button = document.getElementById(
     "search_button"
   ) as HTMLButtonElement | null,
-  searchError = document.getElementById("searchError") as HTMLDivElement | null,
-  is_open = document.getElementById("is_open") as HTMLDivElement | null,
-  all_categories = document.getElementById(
-    "all_categories"
-  ) as HTMLDivElement | null;
+  searchError = document.getElementById("searchError") as HTMLDivElement | null;
+const is_open = document.getElementById("is_open") as HTMLDivElement | null;
+const all_categories = document.getElementById(
+  "all_categories"
+) as HTMLDivElement | null;
 
 setCarouselDelay();
+getLocation();
 
 function setCarouselDelay() {
   setInterval(scrolling, 10000);
@@ -63,7 +65,6 @@ async function getLocation() {
               longitude: longitude,
               limit: "30",
               currency: "USD",
-              open_now: "false",
               lunit: "km",
               lang: "en_US",
             },
@@ -72,72 +73,67 @@ async function getLocation() {
               "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
             },
           });
+
           if (
             slides != undefined &&
             is_open != undefined &&
             all_categories != undefined
           ) {
             slides.innerHTML = data
-              .map(
-                (dta: any) =>
-                  dta?.rating > 4.0 &&
-                  `<div class="c">
+              .map((dta: any) =>
+                dta?.rating > 4.0
+                  ? `<div class="c">
                 <article class="card fade">
                   <img src="${
                     dta.photo?.images.large.url ||
-                    "../image/undraw_Chef_cu0r.png"
+                    "./image/undraw_Chef_cu0r.png"
                   }" alt="${dta.photo?.caption}" />
                   <div class="card_content">
                     <h3 class="card_title">${dta.name || "x"}</h3>
                     <span class="card_subtitle">${dta.address}</span>
-                    <p class="card_description">
-                      ${dta.description || "x"}
-                    </p>
                     <a href="${dta.website}">WEBSITE</a>
                   </div>
                 </article>
               </div>`
-              )
-              .join("");
-
-            is_open.innerHTML = data
-              .map(
-                (dta: any) =>
-                  !dta.is_closed &&
-                  `
-          <div class="isopen" onclick="${dta.website}">
-            <img src="${
-              dta.photo?.images.large.url || "../image/undraw_Chef_cu0r.png"
-            }" alt="${dta.photo?.caption}" />
-            <div class="isopen_content">
-              <h3>${dta.name || "x"}</h3>
-              <p>${dta.address}</p>
-              <p class="desc">${dta.description || "x"}</p>
-              <h4>OPEN</h4>
-            </div>
-          </div>`
+                  : ""
               )
               .join("");
 
             all_categories.innerHTML = data
               .map(
                 (dta: any) =>
-                  `
-          <h2 id="currently_open">ALL CATEGORIES</h2>
-          <div class="isopen" onclick="${dta.website}">
-            <img src="${
-              dta.photo?.images.large.url || "../image/undraw_Chef_cu0r.png"
-            }" alt="${dta.photo?.caption}" />
-            <div class="isopen_content">
-              <h3>${dta.name || "x"}</h3>
-              <p>${dta.address}</p>
-              <p class="desc">${dta.description || "x"}</p>
-              <h4>OPEN</h4>
-            </div>
-          </div>`
+                  `<a href="${
+                    dta.website
+                  } target="_blank""><div class="isopen allcat">
+                <img src="${
+                  dta.photo?.images.large.url || "./image/undraw_Chef_cu0r.png"
+                }" alt="${dta.photo?.caption}" />
+                <div class="isopen_content">
+                  <h3>${dta.name || "x"}</h3>
+                  <p>${dta.address}</p>
+                  <p class="desc">${dta.description || "x"}</p>
+                </div>
+              </div></a>`
               )
               .join("");
-            console.log(data);
+
+            is_open.innerHTML = data
+              .map((dta: any) =>
+                !dta.is_closed
+                  ? `<div class="isopen">
+                <img src="${
+                  dta.photo?.images.large.url || "./image/undraw_Chef_cu0r.png"
+                }" alt="${dta.photo?.caption}" />
+                <div class="isopen_content">
+                  <h3>${dta.name || "x"}</h3>
+                  <p>${dta.address}</p>
+                  <p class="desc">${dta.description || "x"}</p>
+                  <h4><a href="${dta.website}">more details..</a></h4>
+                </div>
+              </div>`
+                  : ""
+              )
+              .join("");
           }
         } catch (error) {
           throw error;
@@ -178,7 +174,8 @@ function GetLatlong(address: string) {
               lang: "en_US",
             },
             headers: {
-              "X-RapidAPI-Key": process.env.API_KEY2,
+              "X-RapidAPI-Key":
+                "8b548f0d36mshb24846c09cc0cfcp1edf30jsn199b6fc034a8",
               "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
             },
           });
@@ -268,5 +265,4 @@ async function searchRestaurant(e: any) {
   }
 }
 
-getLocation();
 search_button?.addEventListener("click", searchRestaurant);

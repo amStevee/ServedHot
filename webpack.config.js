7535f541b05af45e96f6c08e40f156f41147a9fe
amStevee <stevenanongo2@gmail.com>
2023-01-29 06:03:12 +0100

    env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
-const { webpack } = require("webpack");
+const webpack = require("webpack");
+const dotenv = require("dotenv").config({ path: __dirname + "/.env" });
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: "/src/index.ts",
@@ -19,4 +21,12 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.parsed),
+      "process.env.NODE_ENV": JSON.stringify(
+        isDevelopment ? "development" : "production"
+      ),
+    }),
+  ].filter(Boolean),
 };
